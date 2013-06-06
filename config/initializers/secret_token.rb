5883485ba30d35bc198fa17769611d0f10a820e5
Bradley Herman <bradley.t.herman@gmail.com>
2013-06-05 17:12:46 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bradleytherman::Application.config.secret_key_base = '08f37e6db0efe6faca3ead14daf0b154cf7d3b25e81ad03385996cb6d1a9d0d42ec26d7d80a41a570a944c8fb0f7c0a8cb9409e2add7f5b03cd3f44536a92827'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bradleytherman::Application.config.session_store :cookie_store, key: '_bradleytherman_session'
