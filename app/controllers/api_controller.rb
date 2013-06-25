f7bed9aca08faff0994250455b4229bf1b423a45
Bradley Herman <bradley.t.herman@gmail.com>
2013-06-25 16:19:03 -0700

    Hardcoding info for now

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,55 @@
 class ApiController < ApplicationController
   def index
     render json: {
+      verion:       '0.0.1',
+      source:       'http://github.com/bradherman/bradleytherman',
       name:         'Bradley Thomas Herman',
+      description:  'I\'m fairly awesome.',
       age:          27,
-      description:  "I'm fairly awesome.",
-      contact_info: {
+      contact: {
         email: {
           username: 'bradley.t.herman',
           provider: 'gmail.com'
-        }    
-      }
+        },
+        chat: [
+          {
+            type:     'google talk',
+            name:     'bradley.t.herman',
+            provider: 'gmail.com'
+          },
+          {
+            type:     'google talk',
+            name:     'bherman',
+            provider: 'bleacherreport.com'
+          },
+          {
+            type:     'aim',
+            name:     'carinishead',
+            provider: nil
+          }
+        ],
+        social: [
+          {
+            type: 'facebook',
+            href: 'http://www.facebook.com/bradleyherman'
+          },
+          {
+            type: 'linkedin',
+            href: 'http://www.linkedin.com/in/bradleyherman'
+          },
+          {
+            type: 'twitter',
+            href: 'http://www.twitter.com/bradherman'
+          },
+          {
+            type: 'instagram',
+            href: 'http://instagram.com/bradleyherman'
+          }
+        ]
+      },
+      resume:   'coming soon',
+      links:    'coming soon',
+      projects: 'coming soon'
     }
   end
 end
