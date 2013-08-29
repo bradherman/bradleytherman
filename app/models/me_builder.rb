39c40c5a9deebe5c1c659702a201a619270a51f2
Bradley Herman <bradley.t.herman@gmail.com>
2013-08-29 03:08:02 -0700

    Small updates

---
+++ app/models/me_builder.rb
@@ -0,0 +1,63 @@
+class MeBuilder
+  def build
+    {
+      meta: {
+        self: 'http://www.bradleytherman.com'
+      },
+      version:      '0.0.1',
+      source:       'http://github.com/bradherman/bradleytherman',
+      name:         'Bradley Thomas Herman',
+      description:  'I\'m moderately awesome.',
+      age:          27,
+      contact: {
+        email: {
+          username: 'bradley.t.herman',
+          provider: 'gmail.com'
+        },
+        chat: [
+          {
+            type:     'google talk',
+            name:     'bradley.t.herman',
+            provider: 'gmail.com'
+          },
+          {
+            type:     'google talk',
+            name:     'bherman',
+            provider: 'bleacherreport.com'
+          },
+          {
+            type:     'aim',
+            name:     'carinishead',
+            provider: nil
+          }
+        ],
+        social: [
+          {
+            type: 'github',
+            href: 'http://www.github.com/bradherman'
+          },
+          {
+            type: 'facebook',
+            href: 'http://www.facebook.com/bradleyherman'
+          },
+          {
+            type: 'linkedin',
+            href: 'http://www.linkedin.com/in/bradleyherman'
+          },
+          {
+            type: 'twitter',
+            href: 'http://www.twitter.com/bradherman'
+          },
+          {
+            type: 'instagram',
+            href: 'http://instagram.com/bradleyherman'
+          }
+        ]
+      },
+      resume:   nil,
+      links:    [],
+      projects: [],
+      posts:    []
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Bradleytherman::Application.routes.draw do
   root to: 'api#index'
-
+  
   resources :api
 end
