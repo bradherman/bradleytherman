c5e6e73f449ce8cb5dd8c7e975bbcf3738f7c621
Bradley Herman <bradley.t.herman@gmail.com>
2014-08-22 12:28:05 -0700

    Add tagline init

---
+++ config/initializers/taglines.rb
@@ -0,0 +1,8 @@
+TAGLINES = [
+  'I\'m moderately awesome.',
+  'When I\'m not eating ice cream I\'m usually coding or something.',
+  'On a scale of 1 to awesome, I\'m roughly a 6.',
+  'I make a mean turkey sandwich.  Try me.',
+  'I know a little bit about a lot of unimportant things.',
+  'Sometimes, when no one is looking, I like to sleep for 5 days straight.'
+]
